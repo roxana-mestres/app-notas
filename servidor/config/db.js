ce65d70bf746c8ff29cf9bd3964cad8aac9ddb93
Roxana <123836346+roxana-mestres@users.noreply.github.com>
2023-07-21 17:44:58 +0200

    cambios DB para conectar a render

--- servidor/config/db.js
+++ servidor/config/db.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
+
 const conectarBD = async () => {
-    try {
-        //Conexión a la BD utilizando la URL .env "MONGODB_URI".
-        const conectar = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`La base de datos está conectada: ${conectar.connection.host}`);
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    // Conexión a la BD utilizando la URL .env "MONGODB_URI" y las opciones useNewUrlParser y useUnifiedTopology
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("La base de datos está conectada:", mongoose.connection.host);
+  } catch (error) {
+    console.error("Error al conectar con la base de datos:", error.message);
+  }
+};
 
 // Exportar conectarBD para que pueda ser utilizada en otros módulos.
 module.exports = conectarBD;
